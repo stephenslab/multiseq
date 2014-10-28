5deb98f552b515d0bdfe11dac87ae965083f0051
esterpantaleo <esterpantaleo@gmail.com>
2014-10-28 16:21:25 -0500

    little change

--- local/run.multiseq.R
+++ local/run.multiseq.R
@@ -12,19 +12,25 @@ samplesheet     <- args[1]
 region          <- args[2]
 dir.name        <- file.path(args[3])
 fitted.g.file   <- file.path(args[4])
-hub.name <- NULL
-onlyoneend=TRUE #if bam files are paired end only take one of the reads in the pair
+#set fitted g
+set.fitted.g           -> NULL
+set.fitted.g.intercept -> NULL
+if (fitted.g.file!="NA"){
+    load(fitted.g.file)
+    set.fitted.g=ret$fitted.g
+    set.fitted.g.intercept=ret$fitted.g.intercept
+}
 
+
+
+hub.name        <- NULL
+onlyoneend      <- TRUE #if bam files are paired end only take one of the reads in the pair
 prior           <- "nullbiased" 
 lm.approx       <- FALSE #=TRUE #so far we run things with approx=FALSE
                              
 samples         <- read.table(samplesheet, stringsAsFactors=F, header=T)   
-
-#get g
 g               <- factor(samples$Type)
 g               <- match(g, levels(g)) - 1  
-
-#get region
 split_region <- unlist(strsplit(region, "\\:|\\-"))
 chr          <- split_region[1]
 locus.start  <- as.numeric(split_region[2])
@@ -37,22 +43,13 @@ dir.name     <- file.path(dir.name, locus.name)
 dir.create(dir.name)
 
 #get counts
-M <- get.counts(samplesheet, region, onlyoneend=TRUE)
-if (sum(M)<10){
+x <- get.counts(samplesheet, region, onlyoneend=TRUE)
+if (sum(x)<10){
     stop("Total number of reads over all samples is <10. Stopping.")
 }
 
-#set fitted g
-set.fitted.g=NULL
-set.fitted.g.intercept=NULL
-if (fitted.g.file!="NA"){
-    load(fitted.g.file)
-    set.fitted.g=ret$fitted.g
-    set.fitted.g.intercept=ret$fitted.g.intercept
-}
-
 #run multiseq
-res        <- multiseq(M,
+res        <- multiseq(x,
                        g=g,
                        minobs=1,
                        lm.approx=lm.approx,
