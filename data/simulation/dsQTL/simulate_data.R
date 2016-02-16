766407da047b5e0803c87d9d915688b080a80e08
zrxing <zrxing@uchicago.edu>
2016-02-15 22:14:34 -0600

    added code to check multiseq

---
+++ data/simulation/dsQTL/simulate_data.R
@@ -0,0 +1,49 @@
+source("~/multiscale_analysis/src/R/my.utils.R")
+##' `simulate.data' simulate data sets by resampling reads from real data (real.read.counts) for given signals (sig0, sig1).
+##' 
+##' @param seed seed number to set up
+##' @param numGroup0 number of samples for Group0
+##' @param numGroup1 number of samples for Group1
+##' @param real.read.counts a vector of size T (e.g., 1024); t-th element contains number of reads at t-th position; from which we will resample reads for simulation;
+##' @param sig0 a vector of size T (e.g., 1024); t-th element contains probability of sampling read at t-th position in real.read.counts for Group0
+##' @param sig1 a vector of size T (e.g., 1024); t-th element contains probability of sampling read at t-th position in real.read.counts for Group1
+##' @param real.library.read.depth a vector of size M (>1); from whch we will sample library read depth
+##' @param over.dispersion parameter used in sample.from.Binomial.with.Overdispersion; see `my.utils.R' for details. 
+##' @return a list of data, group, library.read.depth; data contains simulated data; matrix of (numGroup0+numGroup1) by T; group contains group indicator for simulated data; a vector of size (numGroup0+numGroup1); library.read.depth contains simulated library read depth; a vector of size (numGroup0+numGroup1)
+simulate.data <- function(seed = 1, numGroup0, numGroup1, sig0, sig1, real.read.counts, real.library.read.depth = NULL, over.dispersion=NULL){
+  
+  genoD = c(rep(0, numGroup0), rep(1, numGroup1))
+  numSam = length(genoD)
+  numBPs = length(sig0)
+  
+  ## phenotype data
+  phenoD = matrix(data=NA, nr= length(genoD), nc = numBPs)
+  
+  ## let's sample!!!
+  set.seed(seed)
+  
+  ## upper and lower bound!
+  trunc.fun = function(x){
+    x = max(0, x)
+    return(min(1,x))
+  }
+  p.sig0 = sapply(sig0, trunc.fun)
+  p.sig1 = sapply(sig1, trunc.fun)
+  
+  ## geno = 0
+  wh0 = which(genoD == 0)
+  if(length(wh0) > 0){
+    phenoD[wh0,] = sample.from.Binomial.with.Overdispersion(num.sam = length(wh0), total.count = real.read.counts, mu.sig = p.sig0, over.dispersion = over.dispersion)
+  }  
+  ## geno = 1
+  wh1 = which(genoD == 1)
+  if(length(wh1) > 0){
+    phenoD[wh1,] = sample.from.Binomial.with.Overdispersion(num.sam = length(wh1), total.count = real.read.counts, mu.sig = p.sig1, over.dispersion = over.dispersion)
+  }
+  if(is.null(real.library.read.depth)){
+    library.read.depth=NULL
+  }else{
+    library.read.depth = sample(real.library.read.depth, numSam, replace = TRUE)
+  }
+  return(list(data = phenoD, group = genoD, library.read.depth = library.read.depth))
+}
---
+++ data/simulation/dsQTL/test_multiseq.R
@@ -0,0 +1,44 @@
+setwd("~/multiseq/data/simulation/dsQTL/")
+source("simulate_data.R")
+
+seed = 1
+numGroup0 = 10
+numGroup1 = 10
+tolerance = 1e-10
+sig0 = scan("alt.sig0", what=double())
+sig1 = scan("alt.sig1", what=double())
+real.DNase.dat = read.table("pheno.dat", as.is = TRUE)
+real.read.counts = ceiling(as.numeric(apply(real.DNase.dat, 2, sum)))
+real.library.read.depth = scan("library.read.depth.dat", what=double())
+
+res = list()
+
+## data with sample size 20 with library read depth 
+res[[1]] = simulate.data(seed = seed, numGroup0 = numGroup0, numGroup1 = numGroup1, sig0 = sig0, sig1= sig1, real.read.counts = real.read.counts, real.library.read.depth = real.library.read.depth, over.dispersion=NULL)
+
+## data with sample size 20 with library read depth, but all samples in group 2 have zero read count.
+res[[2]] = simulate.data(seed = seed, numGroup0 = numGroup0, numGroup1 = numGroup1, sig0 = sig0, sig1= rep(0, length(sig1)), real.read.counts = real.read.counts, real.library.read.depth = real.library.read.depth, over.dispersion=NULL)
+
+## data with sample size 20 without library read depth
+res[[3]] = simulate.data(seed = seed, numGroup0 = numGroup0, numGroup1 = numGroup1, sig0 = sig0, sig1= sig1, real.read.counts = real.read.counts, over.dispersion=NULL)
+
+## data with sample size 20 without library read depth, but all samples in group 2 have zero read count.
+res[[4]] = simulate.data(seed = seed, numGroup0 = numGroup0, numGroup1 = numGroup1, sig0 = sig0, sig1= rep(0, length(sig1)), real.read.counts = real.read.counts, over.dispersion=NULL)
+
+# set.seed(seed)
+# multiseq_out_base = list()
+# for(i in 1:length(res)){
+#   multiseq_out_base[[i]] = multiseq(res[[i]]$data, g = res[[i]]$group, read.depth = res[[i]]$library.read.depth)
+# }
+# 
+# save(multiseq_out_base, file = 'multiseq_out_base.Robj')
+
+set.seed(seed)
+multiseq_out = list()
+for(i in 1:length(res)){
+  multiseq_out[[i]] = multiseq(res[[i]]$data, g = res[[i]]$group, read.depth = res[[i]]$library.read.depth)
+}
+
+load("multiseq_out_base.Robj")
+
+all.equal(multiseq_out_base, multiseq_out, tolerance = tolerance)
